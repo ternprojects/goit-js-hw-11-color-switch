cc9aebdcd7747c32862f7b335eb4cb98fd15a002
Y.Tern <ternxstyle@gmail.com>
2020-06-15 03:07:45 +0300

    added 1-st task

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
 ];
 
-const btnStart = document.querySelector('button[data-action="start"]');
-const btnStop = document.querySelector('button[data-action="sotp"]');
+const refs = {
+  startBtn: document.querySelector('button[data-action="start"'),
+  stopBtn: document.querySelector('button[data-action="stop"'),
+};
 
-btnStart.addEventListener('click', fnStart);
-btnStop.addEventListener('click', fnStop);
+let interval;
 
-setInterval(function fnStart() { 
-}, 1000)
+const setRandomColor = function () {
+    const _color = colors[randomIntegerFromInterval(0, colors.length - 1)];
+    console.log(_color);
+    document.body.style.backgroundColor = _color;
+  };
 
-function fnStop() {
-  clearInterval();
-}
+refs.startBtn.addEventListener('click', (e) =>
+  (interval = interval ? interval : setInterval(() => setRandomColor(), 1000))
+);
+refs.stopBtn.addEventListener("click", (e) => clearInterval(interval));
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-
-console.log(randomIntegerFromInterval(1, 6));
